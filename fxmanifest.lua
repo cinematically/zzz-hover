d71ea9c781dacb4b641b065e6e3acb21c2aa3679
xKomorebi <xKomorebiii@gmail.com>
2024-03-04 00:57:39 -0500

    FXManifest

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Cinematically'
+description 'Hover ability upon button press'
+version '1.0.0'
+
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    'client.lua'
+}
